ea414ed2324d17e6b024574a9a0022d51d00afd7
Filip Volarevic <filip.volarevic@atos.net>
2023-05-18 11:08:37 +0200

    GUI initial look

---
+++ GUI/src/app/red-button/red-button.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-red-button',
+  templateUrl: './red-button.component.html',
+  styleUrls: ['./red-button.component.css']
+})
+export class RedButtonComponent {
+  isButtonPushed = false;
+
+  openPrompt(): void {
+    this.isButtonPushed = true;
+    
+
+    // Delay to reset the pushed state after the animation completes
+    setTimeout(() => {
+      this.isButtonPushed = false;
+      const confirmation = window.alert('I AM ABOUT TO HACK YOU');
+    }, 200);
+    
+  }
+}
